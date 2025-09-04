5bb614780c9ed39369aaf8b36360cb1bf9f14a75
Mustapha Oluwasarasimi <mustaphaoluwadarasimi5@gmail.com>
2025-09-04 16:14:14 +0100

    Linked signup.html and signin.html together

--- JavaScript/Ass/signup.js
+++ JavaScript/Ass/signup.js
@@ -38,5 +38,36 @@ const signUp = () => {
         last_name.value = ''
         e_mail.value = ''
         pass_word.value = ''
+
+        window.location.href = 'signin.html'
+    }
+}
+
+
+
+
+
+const signIn = () => {
+    if (c_email.value === '' || word.value === '') {
+        alert('Please fill all the inputs');
+    } else {
+        const mail = c_email.value.trim();
+        const pass = word.value.trim();
+        console.log(mail, pass);
+
+        console.log(signupArray)
+
+        for (let i = 0; i < signupArray.length; i++) {
+            console.log(signupArray[i].eMail);
+            if (mail === signupArray[i].eMail || pass === signupArray[i].password) {
+                window.location.href = 'dashboard.html'
+            } else {
+                alert("Invalid inputs")
+            }
+        }
+
+        c_email.value = ''
+        word.value = ''
+
     }
 }
